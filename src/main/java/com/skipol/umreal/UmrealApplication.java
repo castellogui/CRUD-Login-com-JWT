df5c734b87ae61e852b2b16412ecb6a125263891
Guilherme Castello <guilherme.brasolin@outlook.com>
2021-12-02 23:31:45 -0300

    first commit

---
+++ src/main/java/com/skipol/umreal/UmrealApplication.java
@@ -0,0 +1,13 @@
+package com.skipol.umreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmrealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmrealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skipol/umreal/UmrealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skipol.umreal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmrealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
