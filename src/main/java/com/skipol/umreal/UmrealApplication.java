715f273c2cd3a39fb23cb6a725b26c33745fe258
Guilherme Castello <guilherme.brasolin@outlook.com>
2021-12-03 10:36:07 -0300

    Implementação de model mapper

--- src/main/java/com/skipol/umreal/UmrealApplication.java
+++ src/main/java/com/skipol/umreal/UmrealApplication.java
@@ -1,13 +1,23 @@
 package com.skipol.umreal;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UmrealApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UmrealApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setAmbiguityIgnored(true);
+
+		return modelMapper;
 	}
 
 }
